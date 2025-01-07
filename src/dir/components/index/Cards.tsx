0ae216393e466652a1aa0fea63a9e3864a66e613
MAC <brytozee@yahoo.com>
2025-01-07 01:24:57 +0100

    indexs

--- src/dir/components/index/Cards.tsx
+++ src/dir/components/index/Cards.tsx
@@ -46,7 +46,7 @@ export const Cards: React.FC = () => {
       </p>
       <h3 className="mt-6 text-gray-400 flex justify-center space-x-2">
         {data &&
-          data?.map((x, y) => (
+          data?.map((x) => (
             <ul className="*:rounded *:border *:border-sky-100 *:bg-sky-50 *:px-2 *:py-0.5 dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10 text-xs">
               <li>{x.data}</li>
             </ul>
